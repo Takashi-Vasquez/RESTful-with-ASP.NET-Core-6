ac23962a96cec4c276e28dfce9ed457b3ba98925
TakashiVásquez <takashivm97@gmail.com>
2023-04-09 20:27:04 -0500

    Default Validation

--- ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
+++ ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
@@ -1,9 +1,21 @@
-﻿namespace WebApiAutores.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiAutores.Entities
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} carácteres")]
         public string Name { get; set; }
+        [Range(18, 100)]
+        [NotMapped]
+        public int Age { get; set; }
+        [CreditCard]
+        public string TarjetaDeCredito { get; set; }
+        [Url]
+        public string URL { get; set; }
         public string Description { get; set; }
         public List<Libro> books { get; set; }
     }
