1ad1ce5ce8bb1aad7cf844a09881c6734736e784
TakashiVásquez <takashivm97@gmail.com>
2023-04-10 09:21:44 -0500

    custom validation

--- ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
+++ ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApiAutores.validations;
 
 namespace WebApiAutores.Entities
 {
@@ -8,8 +9,9 @@ namespace WebApiAutores.Entities
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} carácteres")]
+        [CapitalLetterAttribute]
         public string Name { get; set; }
-        [Range(18, 100)]
+        [Range(18, 120)]
         [NotMapped]
         public int Age { get; set; }
         [CreditCard]
---
+++ ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/validations/CapitalLetterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiAutores.validations
+{
+    public class CapitalLetterAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstLetter = value.ToString()[0].ToString();
+
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                return new ValidationResult("La primera letra debe ser mayúscula");
+            }
+
+            return ValidationResult.Success;
+        }
+
+    }
+}
