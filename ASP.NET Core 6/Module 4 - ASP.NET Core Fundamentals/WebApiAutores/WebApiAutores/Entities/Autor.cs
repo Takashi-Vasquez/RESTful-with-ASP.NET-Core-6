26c14bf08753fb9a8405ac889f2aeb4172af1cf3
TakashiVásquez <takashivm97@gmail.com>
2023-04-10 09:32:55 -0500

    upgrade entities

--- ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
+++ ASP.NET Core 6/Module 4 - ASP.NET Core Fundamentals/WebApiAutores/WebApiAutores/Entities/Autor.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WebApiAutores.validations;
 
 namespace WebApiAutores.Entities
 {
-    public class Autor
+
+    public class Autor : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} carácteres")]
-        [CapitalLetterAttribute]
+        //[CapitalLetter Attribute]
         public string Name { get; set; }
         [Range(18, 120)]
         [NotMapped]
@@ -19,6 +19,28 @@ namespace WebApiAutores.Entities
         [Url]
         public string URL { get; set; }
         public string Description { get; set; }
+
+        public int Menor { get; set; }
+        public int Mayor { get; set; }
         public List<Libro> books { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Name))
+            {
+                var firtletter = Name[0].ToString();
+                if (firtletter != firtletter.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula",
+                        new string[] { nameof(Name) });
+                }
+            }
+
+            if (Menor > Mayor)
+            {
+                yield return new ValidationResult("Estevalor no puede ser más grande que el campo Mayor",
+                   new string[] { nameof(Menor) });
+            }
+        }
     }
 }
